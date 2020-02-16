53b669a99a1ff5cfe7b84185a6095f827bc6ecd3
winston <1054669137@qq.com>
2020-02-16 21:13:10 +0800

    Modify author and copyright information.

--- app/src/main/java/cn/nekocode/camerafilter/filter/HexagonMosaicFilter.java
+++ app/src/main/java/cn/nekocode/camerafilter/filter/HexagonMosaicFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 nekocode
+ * Copyright 2016 winston
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,7 +22,7 @@ import cn.nekocode.camerafilter.MyGLUtils;
 import cn.nekocode.camerafilter.R;
 
 /**
- * @author nekocode (nekocode.cn@gmail.com)
+ * @author winston (1054669137@qq.com)
  */
 public class HexagonMosaicFilter extends CameraFilter {
     private int program;
